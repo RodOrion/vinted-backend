417ada38d425d426b554011cf74d5fa10e97cdae
Rodolphe <rodolphe.of@gmail.com>
2025-08-08 18:34:08 +0200

    cloudy again

--- routes/offer.js
+++ routes/offer.js
@@ -3,7 +3,13 @@ const router = express.Router();
 router.use(express.json());
 const fileUpload = require("express-fileupload");
 const isAuthenticated = require("../middleware/isAuthenticated");
-
+// import de cloudinary
+const cloudinary = require("cloudinary").v2;
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 /** models **/
 const Offer = require("../models/Offer");
